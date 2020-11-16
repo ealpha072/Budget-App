49d5c8c0f5364017ce869a0859429eb9735d5729
ealpha072 <ealpha072@gmail.com>
2020-11-16 11:16:43 +0300

    Coded budget logic

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 ///DOM ELEMENTS
-/*
-const figure = document.querySelector(".figure")
-console.log(figure)*/
-
-///FUNCTIONS
-
+    //inputs
+    const budget = document.querySelector("#budget-amount")
+    const expName = document.querySelector(".exp-name")
+    const expAmt =document.querySelector(".exp-amt")
+    //buttons
+    const submitButton = document.querySelector(".submit-amt")
+    const expButton = document.querySelector(".submit-exp")
+    //headings
+    const budgetHead = document.querySelector(".budget-head")
+    const expHead = document.querySelector(".exp-head")
+    const balHead = document.querySelector(".bal-head")
 
 //EVENT LISTENERS
+submitButton.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let userBudget = budget.value;
+    budgetHead.innerHTML = userBudget;
+
+})
